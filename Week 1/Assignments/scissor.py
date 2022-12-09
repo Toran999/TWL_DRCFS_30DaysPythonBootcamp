174ee6179300f55a1bab8942d30e689ac6b97d9d
Toran <niroulasudeep001@gmail.com>
2022-12-09 19:53:17 +0545

    day 6 week 1

---
+++ Week 1/Assignments/scissor.py
@@ -0,0 +1,45 @@
+import random
+
+score=0
+i=1
+while i<=5:
+    user_move=int(input('Enter your choice: 0 for scissors, 1 for paper, 2 for rock?'))
+    choice=["scissors","paper","rock"]
+    num = random.randint(0,2)
+
+    if user_move==num:
+        print(f"Both you and computer selected {choice[num]}. Its a tie.\n")
+
+    elif user_move==0:
+        if num==1:
+            print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
+            print("you won\n")
+            score += 1
+        else:
+            print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
+            print("you lost\n")
+
+    elif user_move==1:
+        if num==2:
+            print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
+            print("you won\n")
+            score += 1
+        else:
+            print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
+            print("you lost\n")
+
+    elif user_move==2:
+        if num==0:
+            print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
+            print("you won\n")
+            score += 1
+        else:
+            print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
+            print("you lost\n")
+
+    else:
+        print("please select a valid choice.\n")
+
+    i += 1
+
+print(f"You got a total score of {score}")
