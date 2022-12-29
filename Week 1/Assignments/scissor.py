63702c90fddd021c2dde56fcf63e4980e5988554
Toran <niroulasudeep001@gmail.com>
2022-12-29 19:35:16 +0545

    week 4 c

--- Week 1/Assignments/scissor.py
+++ Week 1/Assignments/scissor.py
@@ -1,6 +1,8 @@
 import random
 
 score=0
+c_score=0
+tie=0
 for i in range(5):
     user_move=int(input('Enter your choice: 0 for scissors, 1 for paper, 2 for rock?'))
     choice=["scissors","paper","rock"]
@@ -8,6 +10,7 @@ for i in range(5):
 
     if user_move==num:
         print(f"Both you and computer selected {choice[num]}. Its a tie.\n")
+        tie += 1
 
     elif user_move==0:
         if num==1:
@@ -17,6 +20,7 @@ for i in range(5):
         else:
             print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
             print("you lost\n")
+            c_score += 1
 
     elif user_move==1:
         if num==2:
@@ -26,6 +30,7 @@ for i in range(5):
         else:
             print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
             print("you lost\n")
+            c_score += 1
 
     elif user_move==2:
         if num==0:
@@ -35,9 +40,13 @@ for i in range(5):
         else:
             print(f"you chose {choice[user_move]} and computer chose {choice[num]}")
             print("you lost\n")
+            c_score += 1
 
     else:
         print("please select a valid choice.\n")
+        c_score += 1
+
     print(f"you have {4-i} tries left.\n\n")
 
 print(f"You got a total score of {score}")
+print(f"Computer got a total score of {c_score}")
