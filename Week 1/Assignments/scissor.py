6a66292987fa3ccbecfd63cbd44b2193bd124113
Toran <niroulasudeep001@gmail.com>
2022-12-30 18:54:39 +0545

    final

--- Week 1/Assignments/scissor.py
+++ Week 1/Assignments/scissor.py
@@ -4,7 +4,7 @@ score=0
 c_score=0
 tie=0
 for i in range(5):
-    user_move=int(input('Enter your choice: 0 for scissors, 1 for paper, 2 for rock?'))
+    user_move=int(input('Enter your choice: 0 for scissors, 1 for paper, 2 for rock? '))
     choice=["scissors","paper","rock"]
     num = random.randint(0,2)
 
@@ -49,4 +49,13 @@ for i in range(5):
     print(f"you have {4-i} tries left.\n\n")
 
 print(f"You got a total score of {score}")
-print(f"Computer got a total score of {c_score}")
+print(f"Computer got a total score of {c_score}\n\n")
+print(" ______________________")
+print("|                     | ")
+if c_score>score:
+    print("|    Alas! YOU LOST   |")
+elif score>c_score:
+    print("|  Hooray! YOU WON!!! |")
+else:
+    print("| ITS A TIE.......    |")
+print("|_____________________|")
